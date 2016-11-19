618f04a856f0542b122b781cfa9bdd508a4f408a
opensofias <sofias@radikalismus.com>
2016-11-19 17:36:46 +0100

    basic contructors

--- main.js
+++ main.js
@@ -1,15 +1,39 @@
+var defaultElementSymbols = "0123456789ABCDEF"
+var defaultOperatorSymbols = "*+-/\\%"
+
 class Magma
 {
-	elements
+	constructor ()
+	{
+		this.elements = []
+		this.operations = []
+	}
 
+	elements
 	operations
 	
+	addOperation ()
+	{
+		this.operations.push
+		(
+			new Operation(this,defaultOperatoSymbols[this.operations.length])
+		)
+	}
 
+	addElement ()
+	{
+		this.elements.push(defaultElementSymbols.)
+	}
 }
 
 class Operation
 {
+	constructor (magma, symbol)
+	{
+		this.magma = magma
+		this.symbol = symbol
+	}
+	magma
 	symbol
-
 	table
 }
